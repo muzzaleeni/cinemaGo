89d7e0ed14ae5838c732b99d46606f574d0532a7
Muslim Bayemirov <muslimbayemirov@gmail.com>
2024-06-21 15:19:13 +0500

    Create branches

--- Makefile
+++
@@ -1,7 +0,0 @@
-EXE=my-project
-
-build:
-	go build -o $(EXE) ./cmd/my-project/main.go 
-
-run: build
-	./$(EXE)
--- pkg/liberror/error.go
+++
@@ -1,9 +0,0 @@
-package liberror
-
-import (
-	"errors"
-)
-
-var (
-	ErrNotFound = errors.New("resource not found")
-)
