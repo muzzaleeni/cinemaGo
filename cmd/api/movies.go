2f7314917b1aef6e7f58aa1f0d8ebd44423075a0
Muslim Bayemirov <muslimbayemirov@gmail.com>
2024-06-23 22:11:49 +0500

    Finish chapters up to 4.3

---
+++ cmd/api/movies.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"cinemaGo/internal/data"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
+// return a plain-text placeholder response.
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		// Use the new badRequestResponse() helper.
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+// Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
+// the interpolated "id" parameter from the current URL and include it in a placeholder
+// response.
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		// Use the new notFoundResponse() helper.
+		app.notFoundResponse(w, r)
+		return
+	}
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		// Use the new serverErrorResponse() helper.
+		app.serverErrorResponse(w, r, err)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module cinemaGo
 
 go 1.22.4
 
-require (
-	github.com/julienschmidt/httprouter v1.3.0
-	github.com/lib/pq v1.10.0
-	golang.org/x/crypto v0.24.0
-)
+require github.com/julienschmidt/httprouter v1.3.0
